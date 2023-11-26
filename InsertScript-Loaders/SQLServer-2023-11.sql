71c812e5f4e9a075ed7c2b99e2c28971d4c6473a
Alan Scott <64438147+balanscott@users.noreply.github.com>
2023-11-25 18:16:42 -0600

    2 cleanup and reconcile jupyter notebboks (#3)
    * Updated Jupyter Notebooks file and added a specific SQL Server load script
    
    * Updated Jupyter Notebooks file and added specific files to address 999 record limitation of SQL Server inserts not using BCP utility
    
    * Moved specific insert script load files for Postgres and SQL Server to a specific directory
    
    * Enhanced SQLServer Loader script
    
    * Updates to SQL Server scripts
    
    * Updates to SQL Server scripts for dataexisting and datagenerated records
    
    * Updated SocialSecurityNumber Load Script
    
    * Resolved missing dataexisting load script
    
    * Resolved duplicate datageneration script

---
+++ InsertScript-Loaders/SQLServer-2023-11.sql
@@ -0,0 +1,49 @@
+# SQLCMD -S SERVERNAME -U sa -P St0rangPa55w0rd -i <file>
+-- Reference Data
+SQLCMD -S SERVERNAME -U sa -P St0rangPa55w0rd -i ./ReferenceData/1-refdata_status.sql
+SQLCMD -S SERVERNAME -U sa -P St0rangPa55w0rd -i ./ReferenceData/2-refdata_timezones.sql
+SQLCMD -S SERVERNAME -U sa -P St0rangPa55w0rd -i ./ReferenceData/3-refdata_usstates.sql
+SQLCMD -S SERVERNAME -U sa -P St0rangPa55w0rd -i ./ReferenceData/5-refdata_vendor.sql
+SQLCMD -S SERVERNAME -U sa -P St0rangPa55w0rd -i ./ReferenceData/7-refdata_industrystd.sql
+SQLCMD -S SERVERNAME -U sa -P St0rangPa55w0rd -i ./ReferenceData/8-refdata_sensitivityflag.sql
+SQLCMD -S SERVERNAME -U sa -P St0rangPa55w0rd -i ./ReferenceData/9-refdata_codeset.sql
+SQLCMD -S SERVERNAME -U sa -P St0rangPa55w0rd -i ./ReferenceData/refdata_countries.sql
+SQLCMD -S SERVERNAME -U sa -P St0rangPa55w0rd -i ./ReferenceData/refdata_operationtype.sql
+SQLCMD -S SERVERNAME -U sa -P St0rangPa55w0rd -i ./ReferenceData/refdata_terminologystd.sql
+SQLCMD -S SERVERNAME -U sa -P St0rangPa55w0rd -i ./ReferenceData/refdata_devicetypes.sql
+SQLCMD -S SERVERNAME -U sa -P St0rangPa55w0rd -i ./ReferenceData/refdata_industries.sql
+SQLCMD -S SERVERNAME -U sa -P St0rangPa55w0rd -i ./ReferenceData/refdata_industriestobusiness.sql
+SQLCMD -S SERVERNAME -U sa -P St0rangPa55w0rd -i ./ReferenceData/refdata_legalentities.sql
+SQLCMD -S SERVERNAME -U sa -P St0rangPa55w0rd -i ./ReferenceData/refdata_organization.sql
+SQLCMD -S SERVERNAME -U sa -P St0rangPa55w0rd -i ./ReferenceData/refdata_application.sql
+-- Platform
+SQLCMD -S SERVERNAME -U sa -P St0rangPa55w0rd -i ./Platform/platform_dataattributes.sql
+SQLCMD -S SERVERNAME -U sa -P St0rangPa55w0rd -i ./ReferenceData/10-refdata_datagentypes.sql
+SQLCMD -S SERVERNAME -U sa -P St0rangPa55w0rd -i ./Platform/platform_config_dataattributes.sql
+SQLCMD -S SERVERNAME -U sa -P St0rangPa55w0rd -i ./Platform/platform_config_datastructures.sql
+SQLCMD -S SERVERNAME -U sa -P St0rangPa55w0rd -i ./Platform/platform_config_datastructures_dtl.sql
+-- Data Models
+SQLCMD -S SERVERNAME -U sa -P St0rangPa55w0rd -i ./DataModel/datamodel_domain.sql
+SQLCMD -S SERVERNAME -U sa -P St0rangPa55w0rd -i ./DataModel/datamodel_datatables.sql
+-- DataExisting
+SQLCMD -S SERVERNAME -U sa -P St0rangPa55w0rd -i ../InsertScripts-999Records/DataExisting/dataexisting_ababanking.sql
+SQLCMD -S SERVERNAME -U sa -P St0rangPa55w0rd -i ../InsertScripts-SQLServer/DataExisting/dataexisting_areacode.sql
+SQLCMD -S SERVERNAME -U sa -P St0rangPa55w0rd -i ../InsertScripts-999Records/DataExisting/dataexisting_companies.sql
+SQLCMD -S SERVERNAME -U sa -P St0rangPa55w0rd -i ../InsertScripts-999Records/DataExisting/dataexisting_namefirst.sql
+SQLCMD -S SERVERNAME -U sa -P St0rangPa55w0rd -i ../InsertScripts-999Records/DataExisting/dataexisting_namelast.sql
+SQLCMD -S SERVERNAME -U sa -P St0rangPa55w0rd -i ../InsertScripts-999Records/DataExisting/dataexisting_profession.sql
+SQLCMD -S SERVERNAME -U sa -P St0rangPa55w0rd -i ../InsertScripts-999Records/DataExisting/dataexisting_upccodes.sql
+SQLCMD -S SERVERNAME -U sa -P St0rangPa55w0rd -i ../InsertScripts-999Records/DataExisting/dataexisting_zipcodeus.sql
+-- DataGenerated
+SQLCMD -S SERVERNAME -U sa -P St0rangPa55w0rd -i ../InsertScripts-999Records/DataGenerated/datagenerated_accountnumber.sql
+SQLCMD -S SERVERNAME -U sa -P St0rangPa55w0rd -i ../InsertScripts-999Records/DataGenerated/datagenerated_addresses.sql
+SQLCMD -S SERVERNAME -U sa -P St0rangPa55w0rd -i ../InsertScripts-999Records/DataGenerated/datagenerated_bankaccount.sql
+SQLCMD -S SERVERNAME -U sa -P St0rangPa55w0rd -i ../InsertScripts-999Records/DataGenerated/datagenerated_creditcard.sql
+SQLCMD -S SERVERNAME -U sa -P St0rangPa55w0rd -i ../InsertScripts-999Records/DataGenerated/datagenerated_dateofbirth.sql
+SQLCMD -S SERVERNAME -U sa -P St0rangPa55w0rd -i ../InsertScripts-999Records/DataGenerated/datagenerated_devices.sql
+SQLCMD -S SERVERNAME -U sa -P St0rangPa55w0rd -i ../InsertScripts-999Records/DataGenerated/datagenerated_driverslicenses.sql
+SQLCMD -S SERVERNAME -U sa -P St0rangPa55w0rd -i ../InsertScripts-999Records/DataGenerated/datagenerated_ein.sql
+SQLCMD -S SERVERNAME -U sa -P St0rangPa55w0rd -i ../InsertScripts-999Records/DataGenerated/datagenerated_phonenumbers.sql
+SQLCMD -S SERVERNAME -U sa -P St0rangPa55w0rd -i ../InsertScripts-999Records/DataGenerated/datagenerated_serialnumbers.sql
+SQLCMD -S SERVERNAME -U sa -P St0rangPa55w0rd -i ../InsertScripts-999Records/DataGenerated/datagenerated_socialsecuritynumber.sql
+SQLCMD -S SERVERNAME -U sa -P St0rangPa55w0rd -i ../InsertScripts-999Records/DataGenerated/datagenerated_useridentities.sql
