e63f917f25eb064695e96906123e1f6d318ebe0c
balanscott <balanscott@live.com>
2023-12-31 04:47:26 -0600

    Updates and enhancements to dataloaders - went to single sql import from dozens for ReferenceData

--- InsertScript-Loaders/2024/Postgres-2024-01.sql
+++ InsertScript-Loaders/2024/Postgres-2024-01.sql
@@ -2,31 +2,14 @@
 # "C:\Program Files\PostgreSQL\13\bin\psql.exe" -h localhost -U postgres -d datasynthesis -p 5432 -f ./ReferenceData/1-refdata_status.sql
 # export PGPASSWORD='<password>'
 # /Library/PostgreSQL/14/bin/
-psql -h localhost -U postgres -d datasynthesis -p 5432 -f ./ReferenceData/1-refdata_status.sql
-psql -h localhost -U postgres -d datasynthesis -p 5432 -f ./ReferenceData/2-refdata_timezones.sql
-psql -h localhost -U postgres -d datasynthesis -p 5432 -f ./ReferenceData/3-refdata_usstates.sql
-psql -h localhost -U postgres -d datasynthesis -p 5432 -f ./ReferenceData/5-refdata_vendor.sql
-psql -h localhost -U postgres -d datasynthesis -p 5432 -f ./ReferenceData/7-refdata_industrystd.sql
-psql -h localhost -U postgres -d datasynthesis -p 5432 -f ./ReferenceData/8-refdata_sensitivityflag.sql
-psql -h localhost -U postgres -d datasynthesis -p 5432 -f ./ReferenceData/9-refdata_codeset.sql
-psql -h localhost -U postgres -d datasynthesis -p 5432 -f ./ReferenceData/refdata_countries.sql
-psql -h localhost -U postgres -d datasynthesis -p 5432 -f ./ReferenceData/refdata_operationtype.sql
-psql -h localhost -U postgres -d datasynthesis -p 5432 -f ./ReferenceData/refdata_terminologystd.sql
-psql -h localhost -U postgres -d datasynthesis -p 5432 -f ./ReferenceData/refdata_devicetypes.sql
-psql -h localhost -U postgres -d datasynthesis -p 5432 -f ./ReferenceData/refdata_industries.sql
-psql -h localhost -U postgres -d datasynthesis -p 5432 -f ./ReferenceData/refdata_industriestobusiness.sql
-psql -h localhost -U postgres -d datasynthesis -p 5432 -f ./ReferenceData/refdata_legalentities.sql
-psql -h localhost -U postgres -d datasynthesis -p 5432 -f ./ReferenceData/refdata_organization.sql
-psql -h localhost -U postgres -d datasynthesis -p 5432 -f ./ReferenceData/refdata_application.sql
+-- Reference Data
+psql -h localhost -U postgres -d datasynthesis -p 5432 -f ./1-ReferenceData.sql
 -- Data Models
+-- TBD
 psql -h localhost -U postgres -d datasynthesis -p 5432 -f ./DataModel/datamodel_domain.sql
 psql -h localhost -U postgres -d datasynthesis -p 5432 -f ./DataModel/datamodel_datatables.sql
 -- Platform
-psql -h localhost -U postgres -d datasynthesis -p 5432 -f ./Platform/platform_dataattributes.sql
-psql -h localhost -U postgres -d datasynthesis -p 5432 -f ./ReferenceData/10-refdata_datagentypes.sql
-psql -h localhost -U postgres -d datasynthesis -p 5432 -f ./Platform/platform_config_dataattributes.sql
-psql -h localhost -U postgres -d datasynthesis -p 5432 -f ./Platform/platform_config_datastructures.sql
-psql -h localhost -U postgres -d datasynthesis -p 5432 -f ./Platform/platform_config_datastructures_dtl.sql
+psql -h localhost -U postgres -d datasynthesis -p 5432 -f ./3-PlatformData.sql
 -- DataExisting
 psql -h localhost -U postgres -d datasynthesis -p 5432 -f ./DataExisting/dataexisting_ababanking.sql
 psql -h localhost -U postgres -d datasynthesis -p 5432 -f ./DataExisting/dataexisting_areacode.sql
