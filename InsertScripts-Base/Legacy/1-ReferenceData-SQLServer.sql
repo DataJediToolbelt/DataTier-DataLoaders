4de85af19dc3524bebf8a7f09584372d4a07afc5
balanscott <balanscott@outlook.com>
2024-11-25 23:31:22 -0600

    Updates to Data loaders to enable us to use GUIDs, chars, varchars for various tables throughout platform

---
+++ InsertScripts-Base/Legacy/1-ReferenceData-SQLServer.sql
@@ -0,0 +1,29 @@
+
+SET IDENTITY_INSERT refdata_dataattributes ON;
+INSERT INTO refdata_dataattributes (dataattribute_id, dataattribute_name, sensitivityflag_id, created_date, status_id, created_user, platform_dataattribute_guid, registeredapp_guid, attribute_type) VALUES
+      (1,'Names - Last',3,'2020-08-04 21:51:36.000',1,NULL,NULL,'16a17a48-d09b-11ea-9579-bba61161b25a  ','Upsert'),
+      (2,'Area Code',3,'2020-08-04 21:51:36.000',1,NULL,NULL,'16a17a48-d09b-11ea-9579-bba61161b25a  ','As-Is'),
+      (3,'Address',3,'2020-08-04 21:51:36.000',1,NULL,NULL,'16a17a48-d09b-11ea-9579-bba61161b25a  ','Generator'),
+      (4,'ZipCode US - Includes City & State',3,'2020-08-04 21:51:36.000',1,NULL,NULL,'16a17a48-d09b-11ea-9579-bba61161b25a  ','As-Is'),
+      (5,'Phone Number - US',3,'2020-08-04 21:51:36.000',1,NULL,NULL,'16a17a48-d09b-11ea-9579-bba61161b25a  ','Generator'),
+      (6,'Credit Cards',3,'2020-08-04 21:51:36.000',1,NULL,NULL,'16a17a48-d09b-11ea-9579-bba61161b25a  ','Generator'),
+      (7,'Bank Accounts',1,'2020-08-04 21:51:36.000',1,NULL,NULL,'16a17a48-d09b-11ea-9579-bba61161b25a  ','Generator'),
+      (8,'Date of Birth',3,'2020-08-04 21:51:36.000',1,NULL,NULL,'16a17a48-d09b-11ea-9579-bba61161b25a  ','Generator'),
+      (9,'Drivers License Number',1,'2020-08-04 21:51:36.000',1,NULL,NULL,'16a17a48-d09b-11ea-9579-bba61161b25a  ','Generator'),
+      (10,'Social Security Number',3,'2020-08-04 21:51:36.000',1,NULL,NULL,'16a17a48-d09b-11ea-9579-bba61161b25a  ','Generator'),
+      (11,'UPC Codes',1,'2020-08-04 21:51:36.000',1,NULL,NULL,'16a17a48-d09b-11ea-9579-bba61161b25a  ','As-Is'),
+      (12,'Company Names',1,'2020-08-04 21:51:36.000',1,NULL,NULL,'16a17a48-d09b-11ea-9579-bba61161b25a  ','As-Is'),
+      (13,'Employer Identification Numbers (EIN)',1,'2020-08-04 21:51:36.000',1,NULL,NULL,'16a17a48-d09b-11ea-9579-bba61161b25a  ','As-Is'),
+      (14,'Account Numbers',3,'2020-08-04 21:51:36.000',1,NULL,NULL,'16a17a48-d09b-11ea-9579-bba61161b25a  ','Generator'),
+      (15,'User Identities',3,'2020-08-04 21:51:36.000',1,NULL,NULL,'16a17a48-d09b-11ea-9579-bba61161b25a  ','Generator'),
+      (16,'Bank Routing',3,'2020-08-04 21:51:36.000',1,NULL,NULL,'16a17a48-d09b-11ea-9579-bba61161b25a  ','As-Is'),
+      (17,'Phone Number - International',3,'2020-08-04 21:51:36.000',1,NULL,NULL,'16a17a48-d09b-11ea-9579-bba61161b25a  ','Generator'),
+      (19,'Area Code Intl - IDD',3,'2020-08-04 21:51:36.000',1,NULL,NULL,'16a17a48-d09b-11ea-9579-bba61161b25a  ','As-Is'),
+      (20,'ZipCode Intl ',3,'2020-08-04 21:51:36.000',1,NULL,NULL,'16a17a48-d09b-11ea-9579-bba61161b25a  ','As-Is'),
+      (18,'Names - First',3,'2020-08-04 21:51:36.000',1,NULL,NULL,'16a17a48-d09b-11ea-9579-bba61161b25a  ','Upsert'),
+      (21,'Serial Numbers',6,'2022-06-02 13:59:20.860',1,'NULL','NULL','16a17a48-d09b-11ea-9579-bba61161b25a  ','Generator'),
+      (22,'Regular Expression Based Data',3,'2023-01-18 23:51:30.423',1,'NULL','NULL','16a17a48-d09b-11ea-9579-bba61161b25a  ','Generator'),
+      (23,'Professions',3,'2023-01-19 10:06:59.813',1,'NULL','NULL','16a17a48-d09b-11ea-9579-bba61161b25a  ','As-Is'),
+      (24,'Devices',3,'2023-12-31 04:21:49.731',1,'NULL','NULL ','16a17a48-d09b-11ea-9579-bba61161b25a  ','Generator')
+;
+SET IDENTITY_INSERT refdata_dataattributes OFF;
