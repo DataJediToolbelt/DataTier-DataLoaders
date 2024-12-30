60bdbd179595eca3090a8614d5908b03ae389792
balanscott <balanscott@outlook.com>
2024-12-29 23:16:33 -0600

    Final commits on issue - cleanups and stage setting for future efforts.

--- SQL/SDP-DataAttributes/legacy-attributes_metadata_export_file.sql
+++
@@ -1,3 +0,0 @@
-select 'SSN',dsd.basevalue,dsd.dataattribute_id,dsd.dataattribute_guid
-from datatier_sdp_dataattributes_master dsd 
-where dataattribute_id ='8009f1fc-41d0-4a46-9969-809d7dc5b3cf  '
